0f62bcc038be2d0dbee420ba657afc8b71e15e74
mcpape <mpape234@gmail.com>
2023-11-21 13:58:24 +0100

    init

---
+++ src/test/java/com/example/testingjenkins/TestingJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
